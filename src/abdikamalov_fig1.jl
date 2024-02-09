fa14125ae5cd7f2badc5c2832b59a7be80ea4522
wiktoriatarnopolska <115220668+wiktoriatarnopolska@users.noreply.github.com>
2024-02-09 15:05:05 +0000

    Update abdikamalov_fig1.jl
    got u with the metric declarations

--- src/abdikamalov_fig1.jl
+++ src/abdikamalov_fig1.jl
@@ -50,14 +50,14 @@ end
 m1_a=JohannsenMetric(M=1.0,a=0.0,α13=0.35,ϵ3=0.0)
 d1_at,d1_ab,d1_bt,d1_bb,d1_ct,d1_cb=disk_height(m1_a)
 p1=plot_data(d1_at,d1_ab,d1_bt,d1_bb,d1_ct,d1_cb)
-plot_horizon!(m1, color = :black)
+plot_horizon!(m1_a, color = :black)
 
 
 #a=0.0 α13=0.0
 m2_a=JohannsenMetric(M=1.0,a=0.0,α13=0.0,ϵ3=0.0)
 d2_at,d2_ab,d2_bt,d2_bb,d2_ct,d2_cb=disk_height(m2_a)
 p2=plot_data(d2_at,d2_ab,d2_bt,d2_bb,d2_ct,d2_cb)
-plot_horizon!(m2, color = :black)
+plot_horizon!(m2_a, color = :black)
 
 
 
@@ -66,26 +66,26 @@ m3_a=JohannsenMetric(M=1.0,a=0.0,α13=-0.35,ϵ3=0.0)
 d3_at,d3_ab,d3_bt,d3_bb,d3_ct,d3_cb=disk_height(m3_a)
 
 p3=plot_data(d3_at,d3_ab,d3_bt,d3_bb,d3_ct,d3_cb)
-plot_horizon!(m3, color = :black)
+plot_horizon!(m3_a, color = :black)
 
 #a=0.8 α13=0.35
 
 m4_a=JohannsenMetric(M=1.0,a=0.8,α13=0.35,ϵ3=0.0)
 d4_at,d4_ab,d4_bt,d4_bb,d4_ct,d4_cb=disk_height(m4_a)
 p4=plot_data(d4_at,d4_ab,d4_bt,d4_bb,d4_ct,d4_cb)
-plot_horizon!(m4, color = :black)
+plot_horizon!(m4_a, color = :black)
 
 #a=0.0 α13=0.0
 m5_a=JohannsenMetric(M=1.0,a=0.8,α13=0.0,ϵ3=0.0)
 d5_at,d5_ab,d5_bt,d5_bb,d5_ct,d5_cb=disk_height(m5_a)
 p5=plot_data(d5_at,d5_ab,d5_bt,d5_bb,d5_ct,d5_cb)
-plot_horizon!(m5, color = :black)
+plot_horizon!(m5_a, color = :black)
 
 #a=0.0 α13=-0.35
 m6_a=JohannsenMetric(M=1.0,a=0.8,α13=-0.35,ϵ3=0.0)
 d6_at,d6_ab,d6_bt,d6_bb,d6_ct,d6_cb=disk_height(m6_a)
 p6=plot_data(d6_at,d6_ab,d6_bt,d6_bb,d6_ct,d6_cb)
-plot_horizon!(m6, color = :black)
+plot_horizon!(m6_a, color = :black)
 
 
 #a=0.998 α13=0.35
@@ -93,18 +93,18 @@ plot_horizon!(m6, color = :black)
 m7_a=JohannsenMetric(M=1.0,a=0.998,α13=0.35,ϵ3=0.0)
 d7_at,d7_ab,d7_bt,d7_bb,d7_ct,d7_cb=disk_height(m7_a)
 p7=plot_data(d7_at,d7_ab,d7_bt,d7_bb,d7_ct,d7_cb)
-plot_horizon!(m7, color = :black)
+plot_horizon!(m7_a, color = :black)
 
 #a=0.998 α13=0.0
 m8_a=JohannsenMetric(M=1.0,a=0.998,α13=0.0,ϵ3=0.0)
 d8_at,d8_ab,d8_bt,d8_bb,d8_ct,d8_cb=disk_height(m8_a)
 p8=plot_data(d8_at,d8_ab,d8_bt,d8_bb,d8_ct,d8_cb)
-plot_horizon!(m8, color = :black)
+plot_horizon!(m8_a, color = :black)
 
 #a=0.998 α13=-0.35
 m9_a=JohannsenMetric(M=1.0,a=0.998,α13=-0.35,ϵ3=0.0)
 d9_at,d9_ab,d9_bt,d9_bb,d9_ct,d9_cb=disk_height(m9_a)
 p9=plot_data(d9_at,d9_ab,d9_bt,d9_bb,d9_ct,d9_cb)
-plot_horizon!(m9, color = :black)
+plot_horizon!(m9_a, color = :black)
 
 plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,layout=@layout[p1 p2 p3; p4 p5 p6; p7 p8 p9],title=["a=0.0 α13=0.35" "a=0.0 α13=0.0" "a=0.0 α13=-0.35" "a=0.8 α13=0.35" "a=0.8 α13=0.0" "a=0.8 α13=-0.35" "a=0.998 α13=0.35" "a=0.998 α13=0.0" "a=0.998 α13=-0.35"],plot_title="Abdikamalov Figure 1",size=(1000,1000))
